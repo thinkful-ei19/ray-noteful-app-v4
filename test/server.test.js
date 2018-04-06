d5b9cde7832c2896010d8f8d300b68b678cdc95e
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-04-06 14:21:43 -0700

    updated .travis.yml

--- test/server.test.js
+++ test/server.test.js
@@ -38,54 +38,56 @@ describe('Noteful Server', function() {
     return mongoose.disconnect();
   });
 
-describe('Reality Check', () => {
+  describe('Reality Check', () => {
 
-  it('true should be true', () => {
-    expect(true).to.be.true;
-  });
+    it('true should be true', () => {
+      expect(true).to.be.true;
+    });
 
-  it('2 + 2 should equal 4 (except in 1984)', () => {
-    expect(2 + 2).to.equal(4);
-  });
-
-});
-
-describe('Environment', () => {
-
-  it('NODE_ENV should be "test"', () => {
-    expect(process.env.NODE_ENV).to.equal('test');
-  });
-
-});
-
-describe('Basic Express setup', () => {
-
-  describe('Express static', () => {
-
-    it('GET request "/" should return the index page', () => {
-      return chai.request(app)
-        .get('/')
-        .then(function (res) {
-          expect(res).to.exist;
-          expect(res).to.have.status(200);
-          expect(res).to.be.html;
-        });
+    it('2 + 2 should equal 4 (except in 1984)', () => {
+      expect(2 + 2).to.equal(4);
     });
 
   });
 
-  describe('404 handler', () => {
+  describe('Environment', () => {
 
-    it('should respond with 404 when given a bad path', () => {
-      return chai.request(app)
-        .get('/bad/path')
-        .set('Authorization', `Bearer ${token}`)
-        .catch(err => err.response)
-        .then(res => {
-          expect(res).to.have.status(404);
-        });
+    it('NODE_ENV should be "test"', () => {
+      expect(process.env.NODE_ENV).to.equal('test');
     });
 
   });
+
+  describe('Basic Express setup', () => {
+
+    describe('Express static', () => {
+
+      it('GET request "/" should return the index page', () => {
+        return chai.request(app)
+          .get('/')
+          .then(function (res) {
+            expect(res).to.exist;
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+          });
+      });
+
+    });
+
+    describe('404 handler', () => {
+
+      it('should respond with 404 when given a bad path', () => {
+        return chai.request(app)
+          .get('/bad/path')
+          .set('Authorization', `Bearer ${token}`)
+          .catch(err => err.response)
+          .then(res => {
+            expect(res).to.have.status(404);
+          });
+      });
+
+    });
+  });
 });
-});
+
+  
